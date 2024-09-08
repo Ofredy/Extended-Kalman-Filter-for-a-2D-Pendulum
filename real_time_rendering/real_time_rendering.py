e3188ceabc4a77ad04c0e1265dab841bf94b53c7
Ofredy <orellanafredy96@gmail.com>
2024-09-08 14:18:28 -0500

    rendering optimatization 1

--- real_time_rendering/real_time_rendering.py
+++ real_time_rendering/real_time_rendering.py
@@ -16,43 +16,52 @@ class PendulumRendering:
 
     def _simulation_init(self):
 
-        # system dynamic init
-        self.x_n = np.array([np.pi/2, 0])
+        # System dynamic init
+        self.x_n = np.array([np.pi / 2, 0])  # Initial state (theta, theta_dot)
         self.t = 0
+
+        # Create figure and axis
+        self.fig, self.ax = plt.subplots()
+
+        # Set axis limits
+        self.ax.set_xlim(-L - 1, L + 1)
+        self.ax.set_ylim(-L - 1, 1)
+
+        # Create pendulum arm and bob (pre-allocate)
+        self.line, = self.ax.plot([], [], color='blue', label='Pendulum Arm')  # Line for the arm
+        self.circle = plt.Circle((0, -L), 0.0875, color='red', label='Pendulum Bob')  # Circle for the bob
+        self.ax.add_patch(self.circle)
+
+        self.ax.set_aspect('equal')
+        self.ax.legend(loc='upper left')
 
     def _pendulum_rendering_update(self):
 
-        # rendering position init
+        # Rendering position update
         self.x = L * np.sin(self.x_n[0])
-        self.y = L * np.cos(self.x_n[0])
+        self.y = L * np.cos(self.x_n[0]) 
 
     def _system_step(self, _):
 
+        # Update the pendulum state
         self.x_n = pendulum_next_state(self.x_n, dt)
         self._pendulum_rendering_update()
 
-        plt.cla()
+        # Update the line (pendulum arm)
+        self.line.set_data([0, self.x], [0, self.y])
 
-        # Plot the pendulum rod (line)
-        plt.plot([0, self.x], [0, self.y], color='blue', label='Pendulum Arm')
-
-        # Plot the pendulum bob (circle)
-        circle = plt.Circle((self.x, self.y), 0.1, color='red', label='Pendulum Ball')
-        plt.gca().add_patch(circle)
-
-        # Set axis limits to keep the pendulum in view
-        plt.xlim(-L - 1, L + 1)
-        plt.ylim(-L - 1, 1)
+        # Update the circle (pendulum bob)
+        self.circle.center = (self.x, self.y)
 
     def render(self):
 
-        _ = FuncAnimation(plt.gcf(), self._system_step, interval=50)
+        # Animate without clearing everything on each frame
+        _ = FuncAnimation(self.fig, self._system_step, interval=50)
 
         plt.tight_layout()
         plt.show()
 
 
 if __name__ == "__main__":
-
     rendering = PendulumRendering()
     rendering.render()
