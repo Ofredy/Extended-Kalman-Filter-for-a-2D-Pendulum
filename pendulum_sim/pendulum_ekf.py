variance & std fix, made measurements hz dependent

pendulum_sim/pendulum_ekf.py
@@ -37,12 +37,12 @@ def ekf_update_t(x_prediction_n, P_n, z):
     observation_jacobian_n = observation_jacobian(x_prediction_n)
 
     # calculate kalman gain
-    k_n = P_n @ np.transpose(observation_jacobian_n) @ np.linalg.inv( observation_jacobian_n @ P_n @ np.transpose(observation_jacobian_n) + measurement_noise_std )
+    k_n = P_n @ np.transpose(observation_jacobian_n) @ np.linalg.inv( observation_jacobian_n @ P_n @ np.transpose(observation_jacobian_n) + measurement_noise_variance )
 
     # estimate state
     x_n = x_prediction_n + k_n * ( z - observation_value )
 
     # update estimate covariance
-    P_n = ( np.eye(2) - k_n @ observation_jacobian_n ) @ P_n @ np.transpose( ( np.eye(2) - k_n @ observation_jacobian_n ) ) + (k_n * measurement_noise_std) @ np.transpose(k_n)
+    P_n = ( np.eye(2) - k_n @ observation_jacobian_n ) @ P_n @ np.transpose( ( np.eye(2) - k_n @ observation_jacobian_n ) ) + (k_n * measurement_noise_variance) @ np.transpose(k_n)
 
-    return x_n, P_n
+    return x_n, P_n, k_n

pendulum_sim/pendulum_monte.py
@@ -1,6 +1,9 @@
+# system imports
+import math
+
 # library imports
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 # our imports
@@ -20,17 +23,18 @@ def generate_monte_runs(theta_range=[np.pi/2, -np.pi/2], external_force=False):
     monte_runs = []
 
     # Time array
-    t = np.arange(0, simulation_time, 0.05) if not external_force else np.arange(0, force_simulation_time, 0.05)
+    t_span = [0, simulation_time] if not external_force else [0, force_simulation_time]
+    t = np.arange(0, simulation_time, dt) if not external_force else np.arange(0, force_simulation_time, dt)
 
     for idx in range(NUM_MONTE_RUNS):
 
         if not external_force:
-            solution_n = odeint(pendulum_dynamics, x_0[idx], t, args=(g, L, gamma, None, None))
+            solution_n = solve_ivp(lambda t, y: pendulum_dynamics(y, t, g, L, gamma, None, None), t_span, x_0[idx], t_eval=t)
 
         else:
-            solution_n = odeint(pendulum_dynamics, x_0[idx], t, args=(g, L, gamma, force_mag, force_frequency, True))
+            solution_n = solve_ivp(lambda t, y: pendulum_dynamics(y, t, g, L, gamma, force_mag, force_frequency, True), t_span, x_0[idx], t_eval=t, rtol=1e-8, atol=1e-10)
 
-        monte_runs.append(solution_n)
+        monte_runs.append(np.column_stack((solution_n.y[0], solution_n.y[1])))
 
     return monte_runs
 
@@ -40,8 +44,8 @@ def add_noise_to_monte_runs(monte_runs):
 
         solution_n = monte_runs[idx]
 
-        solution_n[:, 0] = solution_n[:, 0] + np.random.normal(0, process_noise_std, len(solution_n[:, 0]))
-        solution_n[:, 1] = solution_n[:, 1] + np.random.normal(0, process_noise_std, len(solution_n[:, 1]))
+        solution_n[:, 0] = solution_n[:, 0] + np.random.normal(0, math.sqrt(process_noise_variance), len(solution_n[:, 0]))
+        solution_n[:, 1] = solution_n[:, 1] + np.random.normal(0, math.sqrt(process_noise_variance), len(solution_n[:, 1]))
 
         monte_runs[idx] = solution_n
 
@@ -71,14 +75,16 @@ def simulation_init():
 def kalman_filter_simulation(monte_runs, external_force=False):
 
     if not external_force:
-        monte_kalman_estimates = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/dt)))
-        monte_measurement_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/dt)))
-        monte_covaraince_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/dt)))
+        monte_kalman_estimates = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/(1/measurement_hz))))
+        monte_measurement_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/(1/measurement_hz))))
+        monte_covaraince_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/(1/measurement_hz))))
+        monte_k_n = np.zeros(shape=(NUM_MONTE_RUNS, int(simulation_time/(1/measurement_hz))))
 
     else:
-        monte_kalman_estimates = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/dt)))
-        monte_measurement_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/dt)))
-        monte_covaraince_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/dt)))
+        monte_kalman_estimates = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/(1/measurement_hz))))
+        monte_measurement_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/(1/measurement_hz))))
+        monte_covaraince_time_steps = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/(1/measurement_hz))))
+        monte_k_n = np.zeros(shape=(NUM_MONTE_RUNS, int(force_simulation_time/(1/measurement_hz))))
 
     for run_idx in range(NUM_MONTE_RUNS):
 
@@ -88,45 +94,57 @@ def kalman_filter_simulation(monte_runs, external_force=False):
 
         loop_range = int(simulation_time//dt) if not external_force else int(force_simulation_time//dt)
 
+        measurement_counter = 0
+
         for time_step_idx in range(loop_range):
+            
+            measurement_rate_idx = (1/measurement_hz) / dt 
 
-            # make prediction 
-            x_prediction, P_n = ekf_predict_t(x_n, P_n)
+            if time_step_idx % measurement_rate_idx == 0:
 
-            # state update
-            measurement = theta[time_step_idx] + np.random.normal(0, measurement_noise_std)
-            z = L * np.sin(measurement)
+                # make prediction 
+                x_prediction, P_n = ekf_predict_t(x_n, P_n)
 
-            x_n, P_n = ekf_update_t(x_prediction, P_n, z)
+                # state update
+                measurement = theta[time_step_idx] + np.random.normal(0, math.sqrt(measurement_noise_variance))
+                z = L * np.sin(measurement)
 
-            monte_kalman_estimates[run_idx][time_step_idx] = x_n[0][0]
-            monte_measurement_time_steps[run_idx][time_step_idx] = measurement
-            monte_covaraince_time_steps[run_idx][time_step_idx] = P_n[0][0]
+                x_n, P_n, k_n = ekf_update_t(x_prediction, P_n, z)
+
+                monte_kalman_estimates[run_idx][measurement_counter] = x_n[0][0]
+                monte_measurement_time_steps[run_idx][measurement_counter] = measurement
+                monte_covaraince_time_steps[run_idx][measurement_counter] = P_n[0][0]
+                monte_k_n[run_idx][measurement_counter] = np.linalg.norm(k_n)
+
+                measurement_counter += 1
 
     ekf_simulation_summary = { 'ekf_estimates': monte_kalman_estimates,
                                'measurements': monte_measurement_time_steps,
-                               'covariance': monte_covaraince_time_steps }
+                               'covariance': np.sqrt(monte_covaraince_time_steps),
+                               'kalman_gain_norm': monte_k_n }
 
     return ekf_simulation_summary
 
 def plot_kalman_results(monte_runs, ekf_simulation_summary, external_force=False):
 
     # Time array
-    t = np.arange(0, simulation_time, dt) if not external_force else np.arange(0, force_simulation_time, dt)
+    t = np.arange(0, simulation_time, (1/measurement_hz)) if not external_force else np.arange(0, force_simulation_time, (1/measurement_hz))
     plt.figure(1)
+
+    time_indices = np.arange(int(simulation_time/dt)) % int((1/measurement_hz)/dt)  == 0 if not external_force else np.arange(int(force_simulation_time/dt)) % int((1/measurement_hz)/dt)  == 0
 
     monte_kalman_estimates = ekf_simulation_summary['ekf_estimates']
     monte_measurement_time_steps = ekf_simulation_summary['measurements']
 
     for run_idx in range(NUM_MONTE_RUNS):
 
-        theta = monte_runs[run_idx][:, 0]
+        theta = monte_runs[run_idx][:, 0][time_indices]
         kalman_estimates = monte_kalman_estimates[run_idx][:]
         measurement_time_steps = monte_measurement_time_steps[run_idx][:]
 
         # Calculate confidence intervals
-        confidence_interval_upper = kalman_estimates + 3 * measurement_noise_std
-        confidence_interval_lower = kalman_estimates - 3 * measurement_noise_std
+        confidence_interval_upper = kalman_estimates + 3 * math.sqrt(measurement_noise_variance)
+        confidence_interval_lower = kalman_estimates - 3 * math.sqrt(measurement_noise_variance)
 
         if run_idx == 0:
             plt.plot(t, theta, 'g', label='True Theta')
@@ -145,32 +163,33 @@ def plot_kalman_results(monte_runs, ekf_simulation_summary, external_force=False
     plt.ylabel('theta [rad]')
 
     if not external_force:
-        plt.title('Pendulum EKF Monte Results Estimates with Confidence Interval')
+        plt.title('Pendulum EKF Monte Results Estimates with %d [Hz] Measurements' % (measurement_hz))
 
     else:
-        plt.title('Pendulum EKF With External Force Monte Results Estimates with Confidence Interval')
+        plt.title('Pendulum EKF With External Force Monte Results Estimates with %d [Hz] Measurements' % (measurement_hz))
 
     plt.legend()
     plt.grid(True)
 
     if not external_force:
-        plt.savefig("ekf_monte_results.png")
+        plt.savefig("ekf_monte_results_%d_hz.png" % (measurement_hz))
 
     else:
-        plt.savefig("ekf_external_force_monte_results.png")
+        plt.savefig("ekf_external_force_monte_results_%d_hz.png" %(measurement_hz))
 
 def plot_kalman_error(monte_runs, ekf_simulation_summary, external_force=False):
 
     # Time array
-    t = np.arange(0, simulation_time, dt) if not external_force else np.arange(0, force_simulation_time, dt)
+    t = np.arange(0, simulation_time, (1/measurement_hz)) if not external_force else np.arange(0, force_simulation_time, (1/measurement_hz))
     plt.figure(2)
 
+    time_indices = np.arange(int(simulation_time/dt)) % int((1/measurement_hz)/dt)  == 0 if not external_force else np.arange(int(force_simulation_time/dt)) % int((1/measurement_hz)/dt)  == 0
     monte_kalman_estimates = ekf_simulation_summary['ekf_estimates']
     monte_covariance_time_steps = ekf_simulation_summary['covariance']
 
     for run_idx in range(NUM_MONTE_RUNS):
 
-        theta = monte_runs[run_idx][:, 0]
+        theta = monte_runs[run_idx][:, 0][time_indices]
         kalman_estimates = monte_kalman_estimates[run_idx][:]
         covariance = monte_covariance_time_steps[run_idx][:]
 
@@ -182,7 +201,7 @@ def plot_kalman_error(monte_runs, ekf_simulation_summary, external_force=False):
 
         if run_idx == 0:
             plt.plot(t, error, 'k', label='EKF Estimate Error')
-            plt.fill_between(t, confidence_interval_lower, confidence_interval_upper, color='y', alpha=0.5, label='95% Confidence Interval')
+            plt.fill_between(t, confidence_interval_lower, confidence_interval_upper, color='y', alpha=0.5, label='3-σ Uncertainty in Theta')
 
         else:
             plt.plot(t, error, 'k')
@@ -193,19 +212,53 @@ def plot_kalman_error(monte_runs, ekf_simulation_summary, external_force=False):
     plt.ylabel('Error [rad]')
 
     if not external_force:
-        plt.title('EKF Monte Error vs Time With Confidence Interval')
+        plt.title('EKF Monte Error vs Time with %d [Hz] Measurements' % (measurement_hz))
 
     else:
-        plt.title('EKF With External Force Monte Error vs Time With Confidence Interval')
+        plt.title('EKF With External Force Monte Error vs Time  with %d [Hz] Measurements' % (measurement_hz))
     
     plt.legend()
     plt.grid(True)
 
     if not external_force:
-        plt.savefig("ekf_monte_error.png")
+        plt.savefig("ekf_monte_error_%d_hz.png" % (measurement_hz))
 
     else:
-        plt.savefig("ekf_external_force_monte_error.png")
+        plt.savefig("ekf_external_force_monte_error_%d_hz.png" % (measurement_hz))
+
+def plot_kalman_gain(ekf_simulation_summary, figure_num=1, external_force=False):
+
+    plt.figure(figure_num)
+    t = np.arange(0, simulation_time, (1/measurement_hz)) if not external_force else np.arange(0, force_simulation_time, (1/measurement_hz))    
+    
+    for run_idx in range(NUM_MONTE_RUNS):
+
+        k_n = ekf_simulation_summary['kalman_gain_norm'][run_idx][:]
+
+        if run_idx == 0:
+            plt.plot(t, k_n, 'k', label='Kalman Gain Norm')
+
+        else:
+            plt.plot(t, k_n, 'k')
+
+    # Add labels and legend
+    plt.xlabel('time [s]')
+    plt.ylabel('Kalman Gain Norm')
+
+    if not external_force:
+        plt.title('Kalman Gain Norm vs Time')
+
+    else:
+        plt.title('External Force Kalman Gain Norm vs Time')
+    
+    plt.legend()
+    plt.grid(True)
+
+    if not external_force:
+        plt.savefig("kalman_gain_summary_%d_hz.png" % (measurement_hz))
+
+    else:
+        plt.savefig("ex_force_kalman_gain_summary_%d_hz.png" % (measurement_hz))
 
 
 if __name__ == "__main__":
@@ -220,6 +273,7 @@ if __name__ == "__main__":
     # show results
     plot_kalman_results(monte_runs, ekf_simulation_summary)
     plot_kalman_error(monte_runs, ekf_simulation_summary)
+    plot_kalman_gain(ekf_simulation_summary, figure_num=3)
     plt.show()
 
 
@@ -234,4 +288,5 @@ if __name__ == "__main__":
     # show results
     plot_kalman_results(ex_force_monte_runs, ekf_ex_force_simulation_summary, external_force=True)
     plot_kalman_error(ex_force_monte_runs, ekf_ex_force_simulation_summary, external_force=True)
+    plot_kalman_gain(ekf_ex_force_simulation_summary, figure_num=3, external_force=True)
     plt.show()

pendulum_sim/pendulum_simulation.py
@@ -1,3 +1,6 @@
+# system imports
+import math
+
 # library imports
 import numpy as np
 from scipy.integrate import odeint
@@ -13,15 +16,16 @@ gamma = 1 # damping coefficient
 force_mag = 10
 force_frequency = 1
 
-process_noise_std = 0.01
-measurement_noise_std = 0.01
+process_noise_variance = 0.01
+measurement_noise_variance = 0.01
 
 simulation_time = 10 #s
 force_simulation_time = 20 #s
 dt = 0.05 # 50 ms
+measurement_hz = 1
 
-Q = dt * np.array([[process_noise_std, 0],
-                   [ 0, process_noise_std]])
+Q = dt * np.array([[process_noise_variance, 0],
+                   [ 0, process_noise_variance]])
 
 # Define the pendulum dynamics function
 def pendulum_dynamics(y, t, g, L, gamma, force_mag, force_frequency, external_force=False):
@@ -54,8 +58,8 @@ if __name__ == "__main__":
     theta_dot = solution[:, 1]  # Angular velocity
 
     # Add process noise to the results
-    theta_noisy = theta + np.random.normal(0, process_noise_std, len(theta))
-    theta_dot_noisy = theta_dot + np.random.normal(0, process_noise_std, len(theta_dot))
+    theta_noisy = theta + np.random.normal(0, math.sqrt(process_noise_variance), len(theta))
+    theta_dot_noisy = theta_dot + np.random.normal(0, math.sqrt(process_noise_variance), len(theta_dot))
 
     # Plot the results
     plt.figure(figsize=(12, 6))